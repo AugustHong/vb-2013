b4e8b403d1bf8324128f0e471e409f9ac0a63deb
hitler11319 <hitler11319@gmail.com>
2016-09-23 19:12:20 +0800

    9/23 test

---
+++ test1/test1/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim product$, price%
+        Console.Write("請輸出商品名稱：") '輸出文字，但是沒有換行
+        product = Console.ReadLine() '讀取使用者所輸入的一行文字
+
+        Console.Write("請輸出單價：")
+        price = Integer.Parse(Console.ReadLine()) 'integer.parse()將字串轉換為整數
+
+        Console.WriteLine("您買的商品為{0} 單價是{1}，打折後的價格是{2}", product, price.ToString, price * 0.8)
+        'price.tostring將它轉為字串，而writeline是直接印出一行（有換行）
+        Console.ReadLine() '等待使用者輸入中…（可以讓螢幕不會馬上就跑掉，即結束）
+    End Sub
+
+End Module
