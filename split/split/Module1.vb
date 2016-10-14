730657f73260c50b80e519e1a44914a53a16e59e
hitler11319 <hitler11319@gmail.com>
2016-10-14 20:00:20 +0800

    split

---
+++ split/split/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("please input number")
+        Dim s() = Microsoft.VisualBasic.Split(Console.ReadLine(), " ") '要這樣寫(不能先宣告 會錯)，且要打Microsoft.VisualBasic.Split這樣才行
+        For i = 0 To UBound(s)
+            Console.WriteLine(s(i))
+        Next
+        Console.Read()
+    End Sub
+
+End Module
+
