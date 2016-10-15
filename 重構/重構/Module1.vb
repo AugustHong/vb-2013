707f3d36c31fd0b7d6d65f150004a00b4280120a
hitler11319 <hitler11319@gmail.com>
2016-10-15 11:12:32 +0800

    重構

---
+++ 重構/重構/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("(1,2)sum is {0}", sum(1, 2))
+        Console.WriteLine("(1,2，3)sum is {0}", sum(1, 2, 3))
+        Console.Read()
+    End Sub
+
+    Function sum(a As Integer, b As Integer)  '重構（可以用相同函式名稱，但參數的數目不同，他自已會去找）
+        sum = a + b
+    End Function
+
+    Function sum(a As Integer, b As Integer, c As Integer)
+        sum = a + b + c
+    End Function
+
+End Module
