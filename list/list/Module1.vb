600fc7b1bb8a2f6ead78975d336f8209614ce96e
hitler11319 <hitler11319@gmail.com>
2016-10-28 19:59:48 +0800

    list

---
+++ list/list/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+
+
+    Sub main()
+        Dim a As New List(Of Integer) '類似陣列(但是也是可以加入class)
+        'ex: dim b as new list(of member)  -->而member是你弄得class
+
+        a.Add(10)
+        a.Add(30)
+        a.Insert(1, 20) '在第1項加入20
+
+        For i = 0 To 2
+            Console.WriteLine(a(i))
+        Next
+
+        Console.Read()
+    End Sub
+
+
+End Module
+
