58d6e3b384c9febcc16a71d91d1c30f25c2ae13c
hitler11319 <hitler11319@gmail.com>
2016-11-18 19:39:03 +0800

    button

---
+++ ButtonDemo/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.lblShow = New System.Windows.Forms.Label()
+        Me.btnAgain = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 21)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 109)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(128, 21)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(96, 109)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(243, 21)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(96, 109)
+        Me.Button3.TabIndex = 0
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'lblShow
+        '
+        Me.lblShow.AutoSize = True
+        Me.lblShow.Location = New System.Drawing.Point(40, 178)
+        Me.lblShow.Name = "lblShow"
+        Me.lblShow.Size = New System.Drawing.Size(37, 12)
+        Me.lblShow.TabIndex = 1
+        Me.lblShow.Text = "Label1"
+        '
+        'btnAgain
+        '
+        Me.btnAgain.Location = New System.Drawing.Point(70, 234)
+        Me.btnAgain.Name = "btnAgain"
+        Me.btnAgain.Size = New System.Drawing.Size(75, 31)
+        Me.btnAgain.TabIndex = 2
+        Me.btnAgain.Text = "重玩"
+        Me.btnAgain.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(213, 234)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 31)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "離開"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(355, 293)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnAgain)
+        Me.Controls.Add(Me.lblShow)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents lblShow As System.Windows.Forms.Label
+    Friend WithEvents btnAgain As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ ButtonDemo/Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+
+    Dim guess As Integer   ' 要猜的數字，即電腦產生的亂數，亂數範圍1-3
+
+    ' CheckAns程序用來判斷答案是否正確
+    Sub CheckAns(ans As Integer)
+        ' 如果guess(電腦產生的亂數)等於ans(使用者的答案)
+        If guess = ans Then
+            lblShow.Text = "太棒了...答對了!!"
+            ' 如果ans等於1即Button1顯示snowman.jpg影像圖
+            If ans = 1 Then Button1.Image = New Bitmap("snowman.jpg")
+            ' 如果ans等於2即button2顯示snowman.jpg影像圖
+            If ans = 2 Then Button2.Image = New Bitmap("snowman.jpg")
+            ' 如果ans等於3即button3顯示snowman.jpg影像圖
+            If ans = 3 Then Button3.Image = New Bitmap("snowman.jpg")
+        Else
+            lblShow.Text = "不在這裡...猜錯了"
+        End If
+    End Sub
+
+    ' 表單載入時執行
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button1.Text = ""
+        Button2.Text = ""
+        Button3.Text = ""
+        ' 呼叫btnAgain_Click事件處理程序，產生1-3的亂數並指定給guess
+        btnAgain_Click(sender, e)
+        Me.Text = "找雪人遊戲"
+        lblShow.Text = "請問雪人在哪個按鈕"
+        ' 設定lblShow字型大小為14
+        lblShow.Font = New Font(lblShow.Font.FontFamily, 14, FontStyle.Regular)
+    End Sub
+
+    ' 按 [重玩] 鈕執行
+    Private Sub btnAgain_Click(sender As Object, e As EventArgs) Handles btnAgain.Click
+        Dim r As New Random()  ' 建立Random亂數物件r
+        guess = r.Next(1, 4)          ' 產生1-3亂數並指定給guess
+        lblShow.Text = ""
+        ' 設定Button1, Button2, Button3三個按鈕有效
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button3.Enabled = True
+        ' 設定Button1, Button2, Button3三個按鈕不顯示影像圖示
+        Button1.Image = Nothing
+        Button2.Image = Nothing
+        Button3.Image = Nothing
+    End Sub
+
+    ' 按 [離開] 鈕執行
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+
+    ' 按下 Button1 表示指定答案 1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        CheckAns(1)  ' 呼叫CheckAns程序，並傳入答案1
+        Button2.Enabled = False
+        Button3.Enabled = False
+    End Sub
+
+    '  按下 Button2 表示指定答案 2
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        CheckAns(2)   ' 呼叫CheckAns程序，並傳入答案2
+        Button1.Enabled = False
+        Button3.Enabled = False
+    End Sub
+
+    ' 按下 Button3 表示指定答案 3
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        CheckAns(3)     ' 呼叫CheckAns程序，並傳入答案3
+        Button1.Enabled = False
+        Button2.Enabled = False
+    End Sub
+
+End Class
