6b941b63e3a744b2d67c131bdec2088a342aad8b
hitler11319 <hitler11319@gmail.com>
2016-11-25 19:07:50 +0800

    農夫過河

---
+++ 農夫過河/test2/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(24, 70)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 88)
+        Me.ListBox1.TabIndex = 0
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 12
+        Me.ListBox2.Location = New System.Drawing.Point(24, 193)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(120, 88)
+        Me.ListBox2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(185, 97)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "down"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(185, 219)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "up"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(198, 158)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 12)
+        Me.Label1.TabIndex = 4
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(185, 257)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "replay"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(342, 307)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ 農夫過河/test2/Form1.vb
@@ -0,0 +1,77 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim str() As String = {"farman", "wolf", "goat", "菜"}
+
+        ListBox1.Items.AddRange(str)
+        Button3.Enabled = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox2.Enabled = True
+        Button2.Enabled = True
+
+        If ListBox1.SelectedItem.ToString <> "farman" Then
+            ListBox2.Items.Add(ListBox1.SelectedItem.ToString)
+            ListBox1.Items.Remove(ListBox1.SelectedItem.ToString)
+        End If
+
+        ListBox2.Items.Add("farman")
+        ListBox1.Items.Remove("farman")
+
+        judge()
+
+        ListBox1.Enabled = False
+        Button1.Enabled = False
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ListBox1.Enabled = True
+        Button1.Enabled = True
+
+        If ListBox2.SelectedItem.ToString <> "farman" Then
+            ListBox1.Items.Add(ListBox2.SelectedItem.ToString)
+            ListBox2.Items.Remove(ListBox2.SelectedItem.ToString)
+        End If
+
+        ListBox1.Items.Add("farman")
+        ListBox2.Items.Remove("farman")
+
+        judge()
+
+        ListBox2.Enabled = False
+        Button2.Enabled = False
+
+    End Sub
+    Public Sub stoped()
+        Button3.Enabled = True
+        ListBox1.Enabled = False
+        Button1.Enabled = False
+        ListBox2.Enabled = False
+        Button2.Enabled = False
+    End Sub
+    Sub judge()
+        If ListBox1.FindString("wolf") <> -1 And ListBox1.FindString("goat") <> -1 And ListBox1.Items.Count = 2 Then Label1.Text = "wolf eat goat" : stoped()
+        If ListBox1.FindString("菜") <> -1 And ListBox1.FindString("goat") <> -1 And ListBox1.Items.Count = 2 Then Label1.Text = " goat eat 菜" : stoped()
+
+        If ListBox2.FindString("wolf") <> -1 And ListBox2.FindString("goat") <> -1 And ListBox2.Items.Count = 2 Then Label1.Text = "wolf eat goat" : stoped()
+        If ListBox2.FindString("菜") <> -1 And ListBox2.FindString("goat") <> -1 And ListBox2.Items.Count = 2 Then Label1.Text = " goat eat 菜" : stoped()
+
+        If ListBox2.Items.Count = 4 Then Label1.Text = "you win" : stoped()
+
+    End Sub
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Enabled = True
+        ListBox2.Enabled = True
+        Button1.Enabled = True
+        Button2.Enabled = True
+
+        ListBox2.Items.Clear()
+        ListBox1.Items.Clear()
+
+        Label1.Text = ""
+        Form1_Load(sender, e)
+    End Sub
+End Class
