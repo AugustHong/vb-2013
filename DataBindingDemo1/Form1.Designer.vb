3a29dec1c6e4eba154c003084d1328090cb49f79
hitler11319 <hitler11319@gmail.com>
2020-03-14 20:20:16 +0800

    Data Binding

---
+++ DataBindingDemo1/Form1.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意:  以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.txtSalary = New System.Windows.Forms.TextBox()
+        Me.txtPosition = New System.Windows.Forms.TextBox()
+        Me.txtTel = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.cboId = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(21, 134)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(2)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 27
+        Me.DataGridView1.Size = New System.Drawing.Size(459, 140)
+        Me.DataGridView1.TabIndex = 25
+        '
+        'txtSalary
+        '
+        Me.txtSalary.Location = New System.Drawing.Point(283, 94)
+        Me.txtSalary.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtSalary.Name = "txtSalary"
+        Me.txtSalary.Size = New System.Drawing.Size(114, 22)
+        Me.txtSalary.TabIndex = 22
+        '
+        'txtPosition
+        '
+        Me.txtPosition.Location = New System.Drawing.Point(283, 57)
+        Me.txtPosition.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtPosition.Name = "txtPosition"
+        Me.txtPosition.Size = New System.Drawing.Size(114, 22)
+        Me.txtPosition.TabIndex = 21
+        '
+        'txtTel
+        '
+        Me.txtTel.Location = New System.Drawing.Point(54, 91)
+        Me.txtTel.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtTel.Name = "txtTel"
+        Me.txtTel.Size = New System.Drawing.Size(114, 22)
+        Me.txtTel.TabIndex = 24
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(54, 54)
+        Me.txtName.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(114, 22)
+        Me.txtName.TabIndex = 23
+        '
+        'cboId
+        '
+        Me.cboId.FormattingEnabled = True
+        Me.cboId.Location = New System.Drawing.Point(54, 16)
+        Me.cboId.Margin = New System.Windows.Forms.Padding(2)
+        Me.cboId.Name = "cboId"
+        Me.cboId.Size = New System.Drawing.Size(92, 20)
+        Me.cboId.TabIndex = 20
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(248, 97)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(29, 12)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "薪資"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 94)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 12)
+        Me.Label3.TabIndex = 15
+        Me.Label3.Text = "電話"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(248, 59)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(29, 12)
+        Me.Label4.TabIndex = 17
+        Me.Label4.Text = "職稱"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 56)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 12)
+        Me.Label2.TabIndex = 19
+        Me.Label2.Text = "姓名"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 18)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "編號"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(495, 290)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.txtSalary)
+        Me.Controls.Add(Me.txtPosition)
+        Me.Controls.Add(Me.txtTel)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.cboId)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Private WithEvents txtSalary As System.Windows.Forms.TextBox
+    Private WithEvents txtPosition As System.Windows.Forms.TextBox
+    Private WithEvents txtTel As System.Windows.Forms.TextBox
+    Private WithEvents txtName As System.Windows.Forms.TextBox
+    Private WithEvents cboId As System.Windows.Forms.ComboBox
+    Private WithEvents Label5 As System.Windows.Forms.Label
+    Private WithEvents Label3 As System.Windows.Forms.Label
+    Private WithEvents Label4 As System.Windows.Forms.Label
+    Private WithEvents Label2 As System.Windows.Forms.Label
+    Private WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ DataBindingDemo1/Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) _
+     Handles MyBase.Load
+        Using cn As New SqlConnection()
+            cn.ConnectionString = "Data Source=(LocalDB)\v11.0;" & _
+                "AttachDbFilename=|DataDirectory|ch18DB.mdf;" & _
+                "Integrated Security=True;"
+            Dim daEmployee As New SqlDataAdapter _
+             ("SELECT * FROM 員工 ORDER BY 編號 DESC", cn)
+            Dim ds As New DataSet()
+            daEmployee.Fill(ds, "員工")
+            ' ComboBox控制項資料繫結
+            cboId.DataSource = ds
+            cboId.DisplayMember = "員工.編號"
+            ' TextBox控制項資料繫結
+            txtName.DataBindings.Add("Text", ds, "員工.姓名")
+            txtTel.DataBindings.Add("Text", ds, "員工.電話")
+            txtPosition.DataBindings.Add("Text", ds, "員工.職稱")
+            txtSalary.DataBindings.Add("Text", ds, "員工.薪資")
+            ' DataGridView1控制項資料繫結
+            DataGridView1.DataSource = ds
+            DataGridView1.DataMember = "員工"
+        End Using
+    End Sub
+
+End Class
