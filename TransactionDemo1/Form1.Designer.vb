815f2f49b37c590c7bddbb20534c93de104e67e5
hitler11319 <hitler11319@gmail.com>
2020-03-14 20:21:27 +0800

    Transaction

---
+++ TransactionDemo1/Form1.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtIncomeId = New System.Windows.Forms.TextBox()
+        Me.txtIncomeMoney = New System.Windows.Forms.TextBox()
+        Me.txtMyId = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(77, 122)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(2)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 27
+        Me.DataGridView1.Size = New System.Drawing.Size(311, 120)
+        Me.DataGridView1.TabIndex = 14
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(332, 21)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(56, 26)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "轉帳"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtIncomeId
+        '
+        Me.txtIncomeId.Location = New System.Drawing.Point(77, 68)
+        Me.txtIncomeId.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtIncomeId.Name = "txtIncomeId"
+        Me.txtIncomeId.Size = New System.Drawing.Size(76, 22)
+        Me.txtIncomeId.TabIndex = 11
+        '
+        'txtIncomeMoney
+        '
+        Me.txtIncomeMoney.Location = New System.Drawing.Point(224, 68)
+        Me.txtIncomeMoney.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtIncomeMoney.Name = "txtIncomeMoney"
+        Me.txtIncomeMoney.Size = New System.Drawing.Size(76, 22)
+        Me.txtIncomeMoney.TabIndex = 12
+        '
+        'txtMyId
+        '
+        Me.txtMyId.Location = New System.Drawing.Point(77, 26)
+        Me.txtMyId.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtMyId.Name = "txtMyId"
+        Me.txtMyId.Size = New System.Drawing.Size(76, 22)
+        Me.txtMyId.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(22, 122)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 12)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "帳戶明細"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 71)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 12)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "轉入帳號"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(170, 71)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 12)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "轉入金額"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 28)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 12)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "你的帳號"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(412, 264)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtIncomeId)
+        Me.Controls.Add(Me.txtIncomeMoney)
+        Me.Controls.Add(Me.txtMyId)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Private WithEvents Button1 As System.Windows.Forms.Button
+    Private WithEvents txtIncomeId As System.Windows.Forms.TextBox
+    Private WithEvents txtIncomeMoney As System.Windows.Forms.TextBox
+    Private WithEvents txtMyId As System.Windows.Forms.TextBox
+    Private WithEvents Label5 As System.Windows.Forms.Label
+    Private WithEvents Label2 As System.Windows.Forms.Label
+    Private WithEvents Label4 As System.Windows.Forms.Label
+    Private WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ TransactionDemo1/Form1.vb
@@ -0,0 +1,88 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Form1
+    ' 定義ShowData()程序將銀行帳戶資料表所有記錄顯示於DataGridView1上
+    Sub ShowData()
+        Using cn As New SqlConnection()
+            cn.ConnectionString = "Data Source=(LocalDB)\v11.0;" & _
+                "AttachDbFilename=|DataDirectory|ch17DB.mdf;" & _
+                "Integrated Security=True;"
+            Dim daEmployee As New SqlDataAdapter("SELECT * FROM 銀行帳戶", cn)
+            Dim ds As New DataSet()
+            daEmployee.Fill(ds, "銀行帳戶")
+            DataGridView1.DataSource = ds.Tables("銀行帳戶")
+        End Using
+    End Sub
+
+    ' 表單載入時執行此事件
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ShowData()
+    End Sub
+
+    ' 按下 [轉帳] 鈕執行此事件
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' 若使用者餘額小於轉入金額，則執行下列敘述
+        If 0 >= Val(txtIncomeMoney.Text) Then
+            MessageBox.Show("存款不可小於等於零")
+            Exit Sub
+        End If
+
+        Using cn As New SqlConnection()
+            cn.ConnectionString = "Data Source=(LocalDB)\v11.0;" & _
+                     "AttachDbFilename=|DataDirectory|ch17DB.mdf;" & _
+                    "Integrated Security=True;"
+            cn.Open()
+
+            ' 建立SqlCommand物件selectCmd1，用來查詢使用者帳號是否存在
+            Dim selectCmd1 As New SqlCommand("SELECT * FROM 銀行帳戶 WHERE 帳號='" & txtMyId.Text.Replace("'", "''") & "'", cn)
+            ' 建立SqlCommand物件selectCmd1，用來查詢轉入帳號是否存在
+            Dim selectCmd2 As New SqlCommand("SELECT * FROM 銀行帳戶 WHERE 帳號='" & txtIncomeId.Text.Replace("'", "''") & "'", cn)
+            ' 宣告SqlDataReader物件dr1與dr2
+            Dim dr1, dr2 As SqlDataReader
+            ' 傳回SqlDataReader物件dr1，用來查詢使用者帳號是否存在
+            dr1 = selectCmd1.ExecuteReader()
+            If Not dr1.Read() Then   ' 使用者帳號不存在執行下列敘述
+                MessageBox.Show("你的帳號" & txtMyId.Text & "錯誤")
+                Exit Sub
+            End If
+            ' 取得使用者的餘額並定給myMoney
+            Dim myMoney As Integer = Val(dr1.Item("餘額").ToString())
+            dr1.Close()  ' 關閉SqlDataRader物件dr1
+            ' 傳回SqlDataReader物件dr2，用來查詢轉入帳號是否存在
+            dr2 = selectCmd2.ExecuteReader()
+            If Not dr2.Read() Then ' 轉入帳號不存在執行下列敘述
+                MessageBox.Show("轉入帳號" & txtIncomeId.Text & "錯誤")
+                Exit Sub
+            End If
+            dr2.Close()  ' 關閉SqlDataRader物件dr2
+
+            ' 若使用者餘額小於轉入金額，則執行下列敘述
+            If myMoney < Val(txtIncomeMoney.Text) Then
+                MessageBox.Show(txtMyId.Text & "帳號沒這麼多存款")
+                Exit Sub
+            End If
+
+            ' 建立SqlTransaction交易物件tran
+            Dim tran As SqlTransaction = cn.BeginTransaction()
+            Try
+                ' 使用者帳號扣款的SQL語法
+                Dim updateCmd1 As New SqlCommand("UPDATE 銀行帳戶 SET 餘額=餘額-" & txtIncomeMoney.Text & " WHERE 帳號='" & txtMyId.Text.Replace("'", "''") & "'", cn, tran)
+                ' 設定轉入帳號匯款的SQL語法
+                Dim updateCmd2 As New SqlCommand("UPDATE 銀行帳戶 SET 餘額=餘額+" & txtIncomeMoney.Text & " WHERE 帳號='" & txtIncomeId.Text.Replace("'", "''") & "'", cn, tran)
+                updateCmd1.ExecuteNonQuery()
+                updateCmd2.ExecuteNonQuery()
+                tran.Commit() ' 認可交易
+                MessageBox.Show("轉帳成功", "交易成功")
+                txtIncomeId.Text = ""
+                txtIncomeMoney.Text = ""
+                txtMyId.Text = ""
+            Catch ex As Exception
+                tran.Rollback() ' 回復交易
+                MessageBox.Show("轉帳失敗" & vbNewLine & ex.Message, "交易失敗")
+            End Try
+            ShowData()
+        End Using
+    End Sub
+
+End Class
